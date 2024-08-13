13d0c79f1fe1d97e3474dbd0d80ff0b901875b69
Fernando Perez <103344448+Allen845@users.noreply.github.com>
2024-08-13 17:55:27 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// funcion.js
+document.addEventListener("DOMContentLoaded", function () {
+    const ingresarButton = document.getElementById("ingresarButton");
+    
+    ingresarButton.addEventListener("click", function() {
+        window.location.href = "principal.html";
+    });
+});
